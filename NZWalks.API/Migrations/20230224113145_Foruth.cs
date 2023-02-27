b9fb1eecdb65d6b19f16eafa5c2c277c620ffc9f
gadgepiyush <piyushgadge800@gmail.com>
2023-02-27 16:27:15 +0530

    setting up all the database related things

---
+++ NZWalks.API/Migrations/20230224113145_Foruth.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NZWalks.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Foruth : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Walks_WalksDiffuculty_WalkDiffucultyId",
+                table: "Walks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Walks_WalkDiffucultyId",
+                table: "Walks");
+
+            migrationBuilder.DropColumn(
+                name: "WalkDiffucultyId",
+                table: "Walks");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Walks_WalkDifficultyId",
+                table: "Walks",
+                column: "WalkDifficultyId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Walks_WalksDiffuculty_WalkDifficultyId",
+                table: "Walks",
+                column: "WalkDifficultyId",
+                principalTable: "WalksDiffuculty",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Walks_WalksDiffuculty_WalkDifficultyId",
+                table: "Walks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Walks_WalkDifficultyId",
+                table: "Walks");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "WalkDiffucultyId",
+                table: "Walks",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Walks_WalkDiffucultyId",
+                table: "Walks",
+                column: "WalkDiffucultyId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Walks_WalksDiffuculty_WalkDiffucultyId",
+                table: "Walks",
+                column: "WalkDiffucultyId",
+                principalTable: "WalksDiffuculty",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<NZWalksDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalks"));
+});
+
+builder.Services.AddScoped<IRegionRepository, RegionRepository>();
+
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
